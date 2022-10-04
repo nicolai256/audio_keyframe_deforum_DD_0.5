52e38c38dd793a01bf9aabc2168c5316a0440758
nicolai256 <68881396+nicolai256@users.noreply.github.com>
2022-10-04 18:18:25 +0200

    Add files via upload

--- audio_splitter_keyframes.py
+++ audio_splitter_keyframes.py
@@ -227,11 +227,11 @@ if __name__ == "__main__":
     service = AudioKeyframeService(fps=args.fps)
     final_dict = service.process(args.stems,args.file, zoomspeed=args.zoomspeed, speed=args.speed)
     
-    with open("keyframes.json", "w") as fp:
+    with open("audio_splitter_keyframes.json", "w") as fp:
         json.dump(final_dict, fp, indent=2)
         print("")
         print("")
-        print("processing of the keyframes succeeded and exported to keyframes.json")
+        print("processing of the keyframes succeeded and exported to audio_splitter_keyframes.json")
     
 
     #AudioKeyframeService.bpmdetection()
