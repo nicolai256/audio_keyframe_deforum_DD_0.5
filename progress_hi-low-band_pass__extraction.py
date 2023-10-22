05104378255685b28bdf6aaea1af05fe29ef1f0c
Fonk <134219563+FeelTheFonk@users.noreply.github.com>
2023-10-22 21:36:40 +0200

    Improvements/refactor

--- progress_hi-low-band_pass__extraction.py
+++ progress_hi-low-band_pass__extraction.py
@@ -1,91 +1,72 @@
-import cv2
-import numpy as np
-import os
-from os.path import isfile, join
-from tqdm import tqdm
 import argparse
-import librosa
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy import signal
 import csv
+import os
+from scipy import signal
+import librosa
+import numpy as np
 
-import argparse
+def apply_filter(audio_data, sr, low_freq, high_freq=None, filter_type='low'):
+    """Applique un filtre audio en fonction des fréquences et du type de filtre donnés."""
+    b, a = signal.butter(5, low_freq / (sr / 2), filter_type)
+    filtered_data = signal.filtfilt(b, a, audio_data)
+    if high_freq:
+        b, a = signal.butter(5, high_freq / (sr / 2), 'high')
+        filtered_data = signal.filtfilt(b, a, filtered_data)
+    return filtered_data
 
-parser = argparse.ArgumentParser(description='Feature Extraction From MP3')
-parser.add_argument('--path', type=str, help='Output dir for image')
-arguments = parser.parse_args()
-with open('names.csv', newline='') as csvfile:
-     reader = csv.DictReader(csvfile)
-     for row in reader:
-         print(row['tempo'])
-bpm = row
+def read_bpm_from_csv(filename):
+    """Lit le BPM depuis un fichier CSV."""
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"{filename} not found.")
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            return float(row['tempo'])
 
-out_dir = "./features/"
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+def compute_rmse_and_save(audio_data, sr, frame_length, out_file):
+    """Calcule le RMS de l'audio et sauvegarde le résultat."""
+    y_rmse = librosa.feature.rms(audio_data, frame_length=int(frame_length), hop_length=int(frame_length), center=True)[0,:]
+    np.save(out_file, y_rmse)
 
-y, sr = librosa.load(arguments.path)
-set_frame_track = 1/(bpm*16/60)
-frame_length = sr * set_frame_track
+def main():
+    """Fonction principale."""
+    parser = argparse.ArgumentParser(description='Feature Extraction From MP3')
+    parser.add_argument('--path', type=str, help='Path to the audio file')
+    args = parser.parse_args()
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Low Pass Filter
-fc = 150
-w = fc / (sr / 2)
-b, a = signal.butter(5, w, 'low')
-lpf_y = signal.filtfilt(b, a, y)
+    if not os.path.exists(args.path):
+        raise FileNotFoundError(f"{args.path} not found.")
+        
+    out_dir = "./features/"
+    os.makedirs(out_dir, exist_ok=True)
 
-librosa.output.write_wav(out_dir + 'lpf_y.wav', lpf_y, sr)
-lpf_y, sr = librosa.load(out_dir + 'lpf_y.wav')
+    bpm = read_bpm_from_csv('names.csv')
+    y, sr = librosa.load(args.path)
+    
+    # Calcul du cadre temporel pour le découpage de l'audio
+    set_frame_track = 1 / (bpm * 16 / 60)
+    frame_length = sr * set_frame_track
 
-lpf_slice_track=librosa.util.frame(lpf_y, frame_length=int(frame_length), hop_length=int(frame_length))
-lpf_y_rmse = librosa.feature.rms(lpf_y, frame_length=int(frame_length), hop_length=int(frame_length), center=True)
-lpf_y_rmse = lpf_y_rmse[0,:]
+    # Configuration des différents filtres à appliquer
+    filter_configs = [
+        {'name': 'lpf', 'low_freq': 150},
+        {'name': 'bpf', 'low_freq': 200, 'high_freq': 350},
+        {'name': 'hpf', 'low_freq': 500, 'high_freq': 5000}
+    ]
 
-np.save(out_dir + 'lpf_y_rmse.npy', lpf_y_rmse)
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
+    for config in filter_configs:
+        filtered_y = apply_filter(y, sr, config['low_freq'], config.get('high_freq'), 'low' if 'hpf' not in config['name'] else 'high')
+        out_wav = f"{out_dir}{config['name']}_y.wav"
+        out_npy = f"{out_dir}{config['name']}_y_rmse.npy"
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Band Pass Filter
-fc = 350
-w = fc / (sr / 2)
-b, a = signal.butter(5, w, 'low')
-bpf_y = signal.filtfilt(b, a, y)
+        # Sauvegarde de l'audio filtré
+        librosa.output.write_wav(out_wav, filtered_y, sr)
+        
+        # Recharge l'audio pour garantir que les données sont filtrées
+        filtered_y, _ = librosa.load(out_wav)
+        
+        # Calcul et sauvegarde du RMS
+        compute_rmse_and_save(filtered_y, sr, frame_length, out_npy)
 
-fc = 200
-w = fc / (sr / 2)
-b, a = signal.butter(5, w, 'high')
-bpf_y = signal.filtfilt(b, a, bpf_y)
-
-librosa.output.write_wav(out_dir + 'bpf_y.wav', bpf_y, sr)
-bpf_y, sr = librosa.load(out_dir + 'bpf_y.wav')
-
-bpf_slice_track = librosa.util.frame(bpf_y, frame_length=int(frame_length), hop_length=int(frame_length))
-bpf_y_rmse = librosa.feature.rms(bpf_y, frame_length=int(frame_length), hop_length=int(frame_length), center=True)
-bpf_y_rmse=bpf_y_rmse[0,:]
-
-np.save(out_dir + 'bpf_y_rmse.npy', bpf_y_rmse)
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
-#High Pass Filter
-fc = 5000
-w = fc / (sr / 2)
-b, a = signal.butter(5, w, 'low')
-hpf_y = signal.filtfilt(b, a, y)
-
-fc = 500
-w = fc / (22050 / 2)
-b, a = signal.butter(5, w, 'high')
-hpf_y = signal.filtfilt(b, a, hpf_y)
-
-librosa.output.write_wav(out_dir + 'hpf_y.wav', hpf_y, sr)
-hpf_y, sr = librosa.load(out_dir + 'hpf_y.wav')
-
-hpf_slice_track = librosa.util.frame(hpf_y, frame_length=int(frame_length), hop_length=int(frame_length))
-hpf_y_rmse = librosa.feature.rms(hpf_y, frame_length=int(frame_length), hop_length=int(frame_length), center=True)
-hpf_y_rmse = hpf_y_rmse[0,:]
-
-np.save(out_dir + 'hpf_y_rmse.npy', hpf_y_rmse)
-#------------------------------------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
