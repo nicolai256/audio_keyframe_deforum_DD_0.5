be28d88238e5259efd804e58ec83aaff45023b93
Fonk <134219563+FeelTheFonk@users.noreply.github.com>
2023-10-23 12:49:35 +0200

    organize+clean

--- condUI.py
+++ condUI.py
@@ -1,8 +1,6 @@
 from ttkthemes import ThemedTk
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import ttk, filedialog, messagebox
 import os
 import argparse
 import subprocess
@@ -11,24 +9,18 @@ from math import sin, cos, asin, pi
 
 logging.basicConfig(level=logging.INFO)
 
+function_type_options = ['sine', 'cosine', 'abs_sin', 'abs_cos', 'modulus', 'linear', 'triangle', 'fourier']
+
 function_type_combobox = None
 
-def validate_inputs():
-    if not file_path.get():
-        return "Please select an audio file."
+def validate_input(text, text_type, min_val=None):
     try:
-        fps = int(fps_value.get())
-        if fps <= 0:
-            return "FPS must be a positive integer."
+        value = text_type(text)
+        if min_val is not None and value <= min_val:
+            return False
     except ValueError:
-        return "FPS must be an integer."
-    try:
-        intensity = float(intensity_value.get())
-        if intensity <= 0:
-            return "Intensity must be a positive number."
-    except ValueError:
-        return "Intensity must be a number."
-    return None
+        return False
+    return True
 
 def dict_to_str(d):
     return ','.join(f"{key}={value}" for key, value in d.items())
@@ -43,23 +35,14 @@ def validate_advanced_params(params_str):
     return None
 
 def execute_command():
-    validation_result = validate_inputs()
-    if validation_result:
-        messagebox.showerror("Invalid Input", validation_result)
+    if not all([
+        validate_input(file_path.get(), str),
+        validate_input(fps_value.get(), int, 0),
+        validate_input(intensity_value.get(), float, 0),
+        validate_input(advanced_params_value.get(), str)
+    ]):
+        messagebox.showerror("Invalid Input", "Please check your inputs.")
         return
-
-    validation_result = validate_advanced_params(advanced_params_value.get())
-    if validation_result:
-        messagebox.showerror("Invalid Input", validation_result)
-        return
-
-    selected_file = file_path.get()
-    selected_fps = int(fps_value.get())
-    selected_intensity = float(intensity_value.get())
-    selected_function_type = function_type_combobox.get()
- 
-    advanced_params = dict(map(str.strip, param.split("=")) for param in advanced_params_value.get().split(",")) 
-    advanced_params_str = dict_to_str(advanced_params)
 
     python_venv_path = "venv\\Scripts\\python.exe"
     
@@ -68,16 +51,16 @@ def execute_command():
     cmd = [
         python_path,
         "conditional_maths_bpm_keyframes.py",
-        "--file", selected_file,
-        "--fps", str(selected_fps),
-        "--intensity", str(selected_intensity),
-        "--function_type", selected_function_type,
-        "--advanced_params", advanced_params_str
+        "--file", file_path.get(),
+        "--fps", fps_value.get(),
+        "--intensity", intensity_value.get(),
+        "--function_type", selected_function_type.get(),
+        "--advanced_params", advanced_params_value.get()
     ]
 
     if export_all_formulas.get():
-        cmd.append("--export-all-formulas")   
-        
+        cmd.append("--export-all-formulas")
+
     logging.info(f"Executing command: {' '.join(cmd)}")
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, error = process.communicate()
@@ -126,12 +109,30 @@ root.iconbitmap('./favicon.ico')
 file_path = tk.StringVar()
 fps_value = tk.StringVar(value="30")
 intensity_value = tk.StringVar(value="1.0")
-function_type_options = ['sine', 'cosine', 'abs_sin', 'abs_cos', 'modulus', 'linear', 'triangle', 'fourier']
 advanced_params_value = tk.StringVar(value="A=1,P=1,D=0,B=1")
-selected_function_type = tk.StringVar()
-selected_function_type.set('sine')
+selected_function_type = tk.StringVar(value='sine')
 export_all_formulas = tk.BooleanVar(value=False)
-    
-create_widgets()
+
+ttk.Label(root, text="Audio File:").grid(row=0, column=0, sticky="e")
+ttk.Entry(root, textvariable=file_path).grid(row=0, column=1)
+ttk.Button(root, text="Browse", command=lambda: file_path.set(filedialog.askopenfilename(filetypes=[("Audio files", "*.wav *.mp3")]))).grid(row=0, column=2)
+
+ttk.Label(root, text="FPS:").grid(row=1, column=0, sticky="e")
+ttk.Entry(root, textvariable=fps_value).grid(row=1, column=1)
+
+ttk.Label(root, text="Intensity:").grid(row=2, column=0, sticky="e")
+ttk.Entry(root, textvariable=intensity_value).grid(row=2, column=1)
+
+ttk.Label(root, text="Function Type:").grid(row=3, column=0, sticky="e")
+ttk.Combobox(root, textvariable=selected_function_type, values=function_type_options).grid(row=3, column=1)
+
+ttk.Label(root, text="Advanced Params:").grid(row=4, column=0, sticky="e")
+ttk.Entry(root, textvariable=advanced_params_value).grid(row=4, column=1)
+
+tooltip = "A=Amplitude, P=Phase, D=Vertical Shift, B=Second amplitude"
+ttk.Label(root, text=tooltip).grid(row=5, columnspan=3)
+
+ttk.Checkbutton(root, text="Export All Formulas", variable=export_all_formulas).grid(row=6, columnspan=3)
+ttk.Button(root, text="Run Analysis", command=execute_command).grid(row=7, columnspan=3)
 
 root.mainloop()
